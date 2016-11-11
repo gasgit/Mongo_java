be0dc273ce6765b32445252b828213cf8e69599e
gasgit <g00305555@gmit.ie>
2016-11-11 08:53:33 +0000

    clean

--- src/ie/gasgit/Test_Grounds.java
+++ src/ie/gasgit/Test_Grounds.java
@@ -2,6 +2,7 @@ package ie.gasgit;
 
 import com.mongodb.Block;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
@@ -19,20 +20,21 @@ public class Test_Grounds {
 
 
     public static void main(String[] args) {
-
+        // connect to server
         MongoClient mongoClient = new MongoClient("localhost", 27017);
 
+        // connect to database
+        MongoDatabase db = mongoClient.getDatabase("GLEN_TEST");
         System.out.println("Connected to database successfully");
 
-        MongoDatabase db = mongoClient.getDatabase("GLEN_TEST");
-
+        // connect to collection
         MongoCollection<Document> collection = db.getCollection("mycollection");
 
+        // create document
         Document doc = new Document("name", "MongoDB")
                 .append("id", "999")
                 .append("height", 666)
                 .append("eye_col","pink");
-
 
         // insert doc to collection
         collection.insertOne(doc);
@@ -46,6 +48,7 @@ public class Test_Grounds {
         } finally {
             cursor.close();
         }
+
         // count docs in collection
         System.out.println("Total Documents: " + collection.count());
 
@@ -54,8 +57,9 @@ public class Test_Grounds {
         System.out.println(myDoc.toJson());
 
         // find by filter
-//        myDoc = collection.find(eq("name", "glen")).first();
-//        System.out.println(myDoc.toJson());
+        Document myDoc2 = collection.find(eq("name", "MongoDB")).first();
+        System.out.println("here");
+        System.out.println(myDoc2.toJson());
 
         // update doc by field
         collection.updateOne(eq("name", "gordon"), set("name", "glen"));
@@ -63,38 +67,6 @@ public class Test_Grounds {
         // update many
         UpdateResult updateResult = collection.updateMany(eq("name", "glen"), set("name", "gordon"));
         System.out.println("Update Document Count: " + updateResult.getModifiedCount());
-
-
-//        FindIterable<Document> records =  db.getCollection("mycollection").find(new Document("name", "harriot"));
-
-
-//        MongoCursor<Document> iterator = records.iterator();
-//
-//        while(iterator.hasNext()){
-//            Document doc = iterator.next();
-//            System.out.println(doc);
-//        }
-
-
-        //**********************  find all ************************************
-
-//        FindIterable<Document> records = db.getCollection("mycollection").find();
-
-//        while (iterator.hasNext()) {
-//            Document doc = iterator.next();
-//            System.out.println(doc);
-//            String name = doc.getString("name");
-//            String id = doc.get("id").toString();
-//            String height = doc.get("height").toString();
-//            String eye_col = doc.getString("eye_col");
-//
-//            System.out.println("Name: " + name);
-//            System.out.println("ID: " + id);
-//            System.out.println("Height: " + height);
-//            System.out.println("Eye Colour:" + eye_col);
-//        }
-
-        //*******************************************************************************************
 
 
     }
